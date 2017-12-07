44e4d62f7bc0d9cdc9a84877919711ffdf803e66
Anca86 <ancarusu2000@gmail.com>
2017-12-07 22:43:49 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 
 define("_DB_HOST", "localhost");
 define("_DB_USER", "root");
-define("_DB_PASSWORD", "ancar");
-define("_DB_DATABASE", "project1");
+define("_DB_PASSWORD", "password");
+define("_DB_DATABASE", "database");
